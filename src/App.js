205bbace76a3a81a9730bacc54b331fbf680df4b
vivek <viveksinghal746@gmail.com>
2022-07-02 20:13:30 +0530

    add all file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">TIC TAC TOE</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
